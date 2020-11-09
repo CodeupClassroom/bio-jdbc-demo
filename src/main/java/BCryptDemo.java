c3ef1235563f8f4350a1b438542c8d10dd99be3c
Fer <fernandoza@gmail.com>
2020-11-09 16:31:39 -0600

    fix: examples

--- src/main/java/BCryptDemo.java
+++ src/main/java/BCryptDemo.java
@@ -2,7 +2,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class BCryptDemo {
     public static void main(String[] args) {
-        String password = "password123";
+        String password = "Pass$32423";
         String hash = BCrypt.hashpw(password, BCrypt.gensalt());
         System.out.println("password = " + password);
         System.out.println("hash = " + hash);
